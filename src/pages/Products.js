ae1f4633657a37d5c9d8d96653854cb3640a1a39
M-Saleh-N <salehnasser65784@gmail.com>
2025-05-29 23:05:28 +0300

    kept the images in the grid

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,6 +3,11 @@ import React from 'react';
 import maroonRose from '../assets/maroon-roses.jpg';
 import pinkRoses from '../assets/pink-roses.jpg';
 import mixedRoses from '../assets/mixed-roses-bouquet.jpg';
+import photoBouquet from '../assets/photo-bouquet.jpg';
+import blackGlittered from '../assets/black-glittered.jpg';
+import blackRoses from '../assets/black-roses.jpg';
+import moneyBouquet1 from '../assets/blue-money.jpg';
+import cutePink from '../assets/cute-pink.jpg';
 import './Products.css';
 
 const ProductsPage = () => {
@@ -10,6 +15,11 @@ const ProductsPage = () => {
     { id: 1, name: 'Red Mixed Bouquet', price: 2500, imgUrl: mixedRoses },
     { id: 2, name: 'Pink Roses', price: 950, imgUrl: pinkRoses },
     { id: 3, name: 'Maroon Roses', price: 1300, imgUrl: maroonRose },
+    { id: 4, name: 'Photo Bouquet', price: 3600, imgUrl: photoBouquet },
+    { id: 5, name: 'Black Glittered Roses', price: 2500, imgUrl: blackGlittered },
+    { id: 6, name: 'Black Roses', price: 950, imgUrl: blackRoses },
+    { id: 7, name: 'Money Bouquet', price: 1300, imgUrl: moneyBouquet1 },
+    { id: 8, name: 'Pink Forever Roses', price: 3600, imgUrl: cutePink },
   ];
 
   return (
