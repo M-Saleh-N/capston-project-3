1d381940cd47cab06fa35d5100c655a07c36565c
M-Saleh-N <salehnasser65784@gmail.com>
2025-05-29 10:26:30 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
- 
+      <Navbar/>
     </div>
   );
 }
