923f6fa1833ef2e9d8f9884448ad3abd33ed0a49
M-Saleh-N <salehnasser65784@gmail.com>
2025-05-29 23:25:27 +0300

    added new content

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,7 +7,22 @@ import photoBouquet from '../assets/photo-bouquet.jpg';
 import blackGlittered from '../assets/black-glittered.jpg';
 import blackRoses from '../assets/black-roses.jpg';
 import moneyBouquet1 from '../assets/blue-money.jpg';
+import flowerBox from '../assets/flower-box.jpg';
+import maroonForever from '../assets/maroon-forever-roses.jpg';
+import moneyBouquet2 from '../assets/money-bouquet.jpg';
+import jipso from '../assets/red-roses-jipso.jpg';
+import snackBouquet from '../assets/snack-bouquet.jpg';
+import pinkForever from '../assets/pink-forever-roses.jpg';
 import cutePink from '../assets/cute-pink.jpg';
+import katty1 from '../assets/katty1.jpg';
+import katty2 from '../assets/katty2.jpg';
+import katty3 from '../assets/katty3.jpg';
+import mixedMroon from '../assets/mixed-maroon.jpg';
+import mothersDay from '../assets/mothers-day.jpg';
+
+
+
+
 import './Products.css';
 
 const ProductsPage = () => {
@@ -16,10 +31,21 @@ const ProductsPage = () => {
     { id: 2, name: 'Pink Roses', price: 950, imgUrl: pinkRoses },
     { id: 3, name: 'Maroon Roses', price: 1300, imgUrl: maroonRose },
     { id: 4, name: 'Photo Bouquet', price: 3600, imgUrl: photoBouquet },
-    { id: 5, name: 'Black Glittered Roses', price: 2500, imgUrl: blackGlittered },
+    { id: 5, name: 'Mixed Bouquet', price: 2500, imgUrl: jipso },
     { id: 6, name: 'Black Roses', price: 950, imgUrl: blackRoses },
     { id: 7, name: 'Money Bouquet', price: 1300, imgUrl: moneyBouquet1 },
-    { id: 8, name: 'Pink Forever Roses', price: 3600, imgUrl: cutePink },
+    { id: 8, name: 'Pink Forever Roses', price: 3600, imgUrl: pinkForever},
+    { id: 9, name: 'Snack Bouquet', price: 5500, imgUrl: snackBouquet },
+    { id: 10, name: 'Money Bouquet', price: 2500, imgUrl: moneyBouquet2 },
+    { id: 11, name: 'Black Forever Roses', price: 3600, imgUrl: blackGlittered },
+    { id: 12, name: 'Flower Box', price: 3600, imgUrl: flowerBox},
+    { id: 13, name: 'Maroon Forever Roses', price: 3600, imgUrl: maroonForever },
+    { id: 14, name: 'Floral Gift Bag', price: 3600, imgUrl: katty1},
+    { id: 15, name: 'Mixed Roses Bouquet', price: 5500, imgUrl:mixedMroon },
+    { id: 16, name: 'Floral Gift Bag', price: 2500, imgUrl: katty3},
+    { id: 17, name: 'Maroon Forever Roses', price: 3600, imgUrl:  mothersDay},
+    { id: 18, name: 'Pink Mixed Bouquet', price: 3600, imgUrl: cutePink},
+    { id: 19, name: 'Floral Gift Bag', price: 3600, imgUrl: katty2 },
   ];
 
   return (
