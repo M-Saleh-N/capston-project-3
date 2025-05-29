3e1bee6e0e1893d9a3c548f5b14e0919f48f4fa8
M-Saleh-N <salehnasser65784@gmail.com>
2025-05-29 13:57:25 +0300

    added onclick

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 
@@ -6,6 +6,9 @@ import logo from '../assets/Maridadi.png'
 import cart_icon from '../assets/cart_icon.png'
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("products");
+
   return(
     <div className='navbar'>
       <div className='nav-logo'>
@@ -13,16 +16,16 @@ const Navbar = () => {
         <p>MARIDADI BOUQUETS KE</p>
       </div>
       <ul className='nav-menu'>
-        <li>Home</li>
-        <li>Products <hr/></li>
-        <li>Cart</li>
-        <li>Checkout</li>
+        <li onClick={()=>{setMenu("home")}}>Home</li>
+        <li onClick={()=>{setMenu("products")}}>Products <hr/></li>
+        <li onClick={()=>{setMenu("cart")}}>Cart</li>
+        <li onClick={()=>{setMenu("checkout")}}>Checkout</li>
       </ul>
       <div className='nav-login-cart'>
         <button>Login</button>
         <img src={cart_icon} alt=''/>
         <div className='nav-cart-count'>0</div>
-        
+
       </div>
 
     </div>
