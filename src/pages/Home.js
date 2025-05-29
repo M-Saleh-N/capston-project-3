af6830ff872b384197df5e0283bd5fbfa89a49ac
M-Saleh-N <salehnasser65784@gmail.com>
2025-05-29 20:50:07 +0300

    added content pn home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
-const Homie= () => {
-    return (
-        <div>
-        welcome
-        </div>
-    )
-}
-export default Homie;
+import React from 'react';
+import './Home.css'; // Make sure to create this CSS file
+
+const HomePage = () => {
+  return (
+    <div className="bg">
+      <div className="title">Welcome to Maridadi Bouquets</div>
+      <div className="center">
+        <button className="btn">Explore</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
