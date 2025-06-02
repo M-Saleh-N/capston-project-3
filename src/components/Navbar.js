91538feab16e751937f8c95cb068ba713998c84d
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-02 13:55:01 +0300

    added more info

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
-
-import logo from '../assets/Maridadi.png'
-import cart_icon from '../assets/cart_icon.png'
+import './Navbar.css';
+import logo from '../assets/Maridadi.png';
+import cart_icon from '../assets/cart_icon.png';
 
 const Navbar = () => {
+  const { cartItems } = useContext(CartContext);
+  const total = cartItems.reduce((sum, item) => sum + item.quantity, 0);
 
-    const [menu,setMenu] = useState("products");
-
-  return(
+  return (
     <div className='navbar'>
       <div className='nav-logo'>
-        <img src={logo} alt=''/>
+        <img src={logo} alt='' />
         <p>MARIDADI BOUQUETS KE</p>
       </div>
       <ul className='nav-menu'>
-        <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("products")}}>Products{menu==="products"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("cart")}}>Cart{menu==="cart"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("checkout")}}>Checkout{menu==="checkout"?<hr/>:<></>}</li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/checkout">Checkout</Link></li>
       </ul>
       <div className='nav-login-cart'>
         <button>Login</button>
-        
-        <img src={cart_icon} alt='cart'/>
-        <div className='nav-cart-count'>0</div>
-
+        <Link to="/cart" className="cart-icon-link">
+          <img src={cart_icon} alt='cart' />
+          <div className='nav-cart-count'>{total}</div>
+        </Link>
       </div>
-
     </div>
-  )
-
-}
-
+  );
+};
 
 export default Navbar;
