18e7bd052c89eb5c8503acca4e498b17b2b7660b
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-02 16:29:56 +0300

    added checkout link to the cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useCart } from '../context/CartContext';
-import { Link } from 'react-router-dom';
-import './Cart.css'
+import React from "react";
+import { useCart } from "../context/CartContext";
+import { Link } from "react-router-dom";
+import "./Cart.css";
 
 const Cart = () => {
   const { cartItems, removeFromCart } = useCart();
@@ -10,33 +10,45 @@ const Cart = () => {
 
   return (
     <div className="cart-container">
-  <h2 className="cart-title">Your Cart</h2>
-  {cartItems.length === 0 ? (
-    <p className="empty-message">Your cart is empty.</p>
-  ) : (
-    <>
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <div key={item.id} className="cart-item">
-            <img src={item.imgUrl} alt={item.name} />
-            <div className="cart-details">
-              <h4>{item.name}</h4>
-              <p>Price: KES {item.price}</p>
-              <p>Quantity: {item.quantity}</p>
-            </div>
-            <button onClick={() => removeFromCart(item.id)} className="remove-btn">
-              Remove
-            </button>
+      <h2 className="cart-title">Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p className="empty-message">Your cart is empty.</p>
+      ) : (
+        <>
+          <div className="cart-items">
+            {cartItems.map((item) => (
+              <div key={item.id} className="cart-item">
+                <img src={item.imgUrl} alt={item.name} />
+                <div className="cart-details">
+                  <h4>{item.name}</h4>
+                  <p>Price: KES {item.price}</p>
+                  <p>Quantity: {item.quantity}</p>
+                </div>
+                <button
+                  onClick={() => removeFromCart(item.id)}
+                  className="remove-btn"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="cart-total">
-        Total: KES {cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}
-      </div>
-    </>
-  )}
-</div>
+          <div className="cart-total">
+            Total: KES{" "}
+            {cartItems.reduce(
+              (total, item) => total + item.price * item.quantity,
+              0
+            )}
+            
+             <Link to="/checkout" className="checkout-button">
+            Proceed to Checkout
+          </Link>
 
+          </div>
+        </>
+        
+      )}
+    </div>
   );
 };
 
