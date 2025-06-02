a0f26a7416a658ec2ebfb8b854bc4aa3ffc2fa71
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-02 17:35:34 +0300

    updated

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,6 +7,7 @@ const Cart = () => {
   const { cartItems, removeFromCart } = useCart();
 
   const total = cartItems.reduce((sum, item) => sum + item.price, 0);
+  
 
   return (
     <div className="cart-container">
