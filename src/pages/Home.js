80657da7cd7005a1424b9aeb149f5a74913893ca
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-02 21:53:02 +0300

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,3 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
-
-const HomePage = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div className="bg">
-      <h1 className="title">Welcome to Maridadi Bouquets</h1>
-
-      <div className="center">
-        <button className="btn" onClick={() => navigate('/products')}>
-          <p>Explore Products</p>
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default HomePage;
-
-
-
-
+c
 
 
